507a081980306551477cbc59cf8da7e099cf67c4
rahim4shanewaz <soumiq444@gmail.com>
2022-08-07 15:31:17 +0600

    solve

--- function-problem.js
+++ function-problem.js
@@ -1,5 +1,6 @@
 
 // inch to feet problem
+/*
 function inchToFeet(inch){
     var givenData = inch;
     var result = givenData/12;
@@ -20,9 +21,11 @@ function evenOdd(number){
 }
 evenOdd(25);
 
+*/
+
 // array elemnt problem
 
-function getArraySum(numbers){
+/*function getArraySum(numbers){
     let sum = 0;
     for(let i = 0; i < numbers.length; i++){
         const index = i;
@@ -33,4 +36,40 @@ function getArraySum(numbers){
     return sum;
 }
 const myNumber = [67, 45, 33, 66, 12, 45, 55];
-getArraySum(myNumber,);
+getArraySum(myNumber);
+*/
+
+
+// array odd elemnt find and sum
+
+function arrayOdd(numbers){
+    const sum = 0;
+    const oddNumber = [];
+    for(let i=0; i < numbers.length; i++){
+        const index = i;
+        const elemnt = numbers[i];
+        if (elemnt % 2 !== 0) {
+            console.log(index, elemnt);
+            oddNumber.push( elemnt);
+        }
+    }
+    return oddNumber;
+}
+const anyNumbers = [67, 2, 56, 78, 77, 56, 34, 23, 99]
+arrayOdd(anyNumbers);
+const oddNumber = arrayOdd(anyNumbers);
+console.log('Arry of odd numbers:', oddNumber);
+
+function getArraySum(numbersofsum){
+    let sumOfOdd = 0;
+    for(let i = 0; i < numbersofsum.length; i++){
+        const index = i;
+        const elemnt = numbersofsum[index];
+        sumOfOdd =sumOfOdd + elemnt;
+        console.log(index, elemnt)
+    }
+
+    console.log( 'Sum of odd numbers:', sumOfOdd);
+}
+
+getArraySum(oddNumber);
