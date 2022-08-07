b8bfaf600f647a986d89736b3de0b92f65b8bb1d
rahim4shanewaz <soumiq444@gmail.com>
2022-08-07 14:42:46 +0600

    solve

--- function-problem.js
+++ function-problem.js
@@ -1,6 +1,21 @@
+
+// inch to feet problem
 function inchToFeet(inch){
     var givenData = inch;
     var result = givenData/12;
     return result;
 }
 console.log("Result is" +" " + inchToFeet(48));
+
+//even odd find
+
+function evenOdd(number){
+    var num = number;
+    if (num%2 == 0) {
+        console.log('Given number is Even') 
+    }
+    else{
+        console.log('Given number is Odd')
+    }
+}
+evenOdd(25);
